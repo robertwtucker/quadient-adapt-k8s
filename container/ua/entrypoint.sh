5846ace922c8b9a86f1f54132290a91f24fdd468
Robert Tucker <r.tucker@quadient.com>
2024-10-23 11:09:38 -0400

    fix: link files before populating shared vol
    Signed-off-by: Robert Tucker <r.tucker@quadient.com>

--- container/ua/entrypoint.sh
+++ container/ua/entrypoint.sh
@@ -6,11 +6,14 @@ set -e
 
 : ${ADAPT_PATH:=/opt/adapt}
 
-## Create symbolic links to license and configuration files
+# Create symbolic links to license and configuration files provided
+# in the etc directory.
 for FILE in "${ADAPT_PATH}"/etc/*; do
 	ln -sf "${FILE}" "${ADAPT_PATH}"/ua/$(basename ${FILE})
 done
 
+# If required, populate the shared volume with the contents of
+# of the ua directory.
 if [ "$(basename $1)" == "share" ]; then
 	cp -RL "${ADAPT_PATH}"/ua/* "${ADAPT_PATH}"/share
 else
